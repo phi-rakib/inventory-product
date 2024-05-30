01230758ef5e1886c6bb400aa77ba49c7bc0dff6
rakib <phi.rakib@gmail.com>
2024-05-30 22:48:02 +0600

    wrote all the routes instead of resourc routes for better readability

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use PhiRakib\InventoryProduct\Controllers\BrandController;
 
-Route::resource('brands', BrandController::class);
+Route::get('brands', [BrandController::class, 'index'])->name('brands.index');
+Route::get('brands/{id}', [BrandController::class, 'show'])->name('brands.show');
+Route::post('brands', [BrandController::class, 'store'])->name('brands.store');
+Route::put('brands/{id}', [BrandController::class, 'update'])->name('brands.update');
+Route::delete('brands/{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
